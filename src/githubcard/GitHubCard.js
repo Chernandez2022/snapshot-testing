659e6c0fd711adeff915adfd5744f4fff54fed9a
Chernandez2022 <hcarlos0816@gmail.com>
2022-12-27 01:15:10 -0500

    finalized githubcard and snapshot test

--- src/githubcard/GitHubCard.js
+++ src/githubcard/GitHubCard.js
@@ -1,4 +1,20 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-function GitHubCard() {}
+function GitHubCard() {
+  return (
+    <div className="card">
+      <Card style={{ width: "10rem" }}>
+        {Chernandez2022}
+        <Card.Img variant="top" />
+        {}
+        <Card.Body>
+          <Card.Title>Chernandez2022</Card.Title>
+          <Card.Text>Through failure I am finally awake</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default GitHubCard;
